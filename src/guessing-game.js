68b9f8aab0ff84cc294f4c82ee63e0348c348e2f
tatidem <tdemyanova@mail.ru>
2023-10-29 03:00:53 +0300

    feat: solve the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.solution = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.solution = Math.round((this.max - this.min) / 2) + this.min;;
+        return this.solution;
     }
 
     lower() {
-
+        this.setRange(this.min, this.solution);
     }
 
     greater() {
-
+        this.setRange(this.solution, this.max);
     }
 }
+
 
 module.exports = GuessingGame;
